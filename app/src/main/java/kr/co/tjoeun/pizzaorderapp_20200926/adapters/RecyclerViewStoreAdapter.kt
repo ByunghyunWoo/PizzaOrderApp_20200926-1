c12930090b3ef026e8445d231386a1625089f2dd
wbh <showmana@naver.com>
2020-10-05 12:23:54 +0900

    해야할 것: 리싸이클러뷰에 클릭이벤트 넣기

--- app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/adapters/RecyclerViewStoreAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/adapters/RecyclerViewStoreAdapter.kt
@@ -1,64 +1,68 @@
 package kr.co.tjoeun.pizzaorderapp_20200926.adapters
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.util.Size
-
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.fragment_pizza_store_list_view.*
 import kr.co.tjoeun.pizzaorderapp_20200926.R
 import kr.co.tjoeun.pizzaorderapp_20200926.ViewStoreDetailActivity
 import kr.co.tjoeun.pizzaorderapp_20200926.datas.Store
-import kr.co.tjoeun.pizzaorderapp_20200926.fragments.PizzaStoreRecyclerViewFragment
-import java.nio.file.Files.size
 
 class RecyclerViewStoreAdapter(
     val mContext: Context,
-    val resId:Int,
-    val mStore:ArrayList<Store>):RecyclerView.Adapter<RecyclerViewStoreAdapter.RecyclerViewHolder>() {
-
+    val resId: Int,
+    val mStore: ArrayList<Store>
+) : RecyclerView.Adapter<RecyclerViewStoreAdapter.RecyclerViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-        val view = LayoutInflater.from(mContext).inflate(R.layout.store_recycler_item,parent,false)  // 너였냐?
+        val view = LayoutInflater.from(mContext)
+            .inflate(R.layout.store_recycler_item, parent, false)  // 너였냐?
         return RecyclerViewHolder(view)
+//            .apply {
+//                itemView.setOnClickListener {
+//
+//                    val clickedStore: Int = adapterPosition
+//                    val myIntent = Intent(mContext, ViewStoreDetailActivity::class.java)
+//                    myIntent.putExtra("storeData", clickedStore)
+//
+//                    startActivity(myIntent)
+//                }
+//            }
+
+                    //???
+
+
     }
 
     override fun getItemCount(): Int {
 
 
-        return mStore.size   // ? 개간단 ㅋㅋ
+        return mStore.size   //
     }
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-
         holder.bind(mStore[position], mContext)
     }
 
 
-
-    inner class RecyclerViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!){
+    inner class RecyclerViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
 
         val logoImg = itemView?.findViewById<ImageView>(R.id.logoImg)
         val brandNameTxt = itemView?.findViewById<TextView>(R.id.brandNameTxt)
 
-        fun bind(storeData : Store, mcontext:Context){
+
+        fun bind(storeData: Store, mcontext: Context) {
             brandNameTxt?.text = storeData.brandName
             Glide.with(mContext).load(storeData.logoUrl).into(logoImg!!)
         }
-
-
-
-
-
     }
-
 }
--- app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/fragments/PizzaStoreRecyclerViewFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/fragments/PizzaStoreRecyclerViewFragment.kt
@@ -1,7 +1,6 @@
 package kr.co.tjoeun.pizzaorderapp_20200926.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,7 +14,7 @@ class PizzaStoreRecyclerViewFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<Store>()
 
-    lateinit var mRecyclerViewStoreAdapter : RecyclerViewStoreAdapter
+    lateinit var mRecyclerViewStoreAdapter: RecyclerViewStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,29 +23,116 @@ class PizzaStoreRecyclerViewFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_pizza_store_recycler_view, container, false)
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        mPizzaStoreList.add(Store("피자헛", "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
-        mPizzaStoreList.add(Store("파파존스", "1577-8080", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"))
-        mPizzaStoreList.add(Store("미스터피자", "1577-0077", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"))
-        mPizzaStoreList.add(Store("도미노피자", "1577-3082", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"))
-        mPizzaStoreList.add(Store("피자헛", "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
-        mPizzaStoreList.add(Store("파파존스", "1577-8080", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"))
-        mPizzaStoreList.add(Store("미스터피자", "1577-0077", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"))
-        mPizzaStoreList.add(Store("도미노피자", "1577-3082", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"))
-        mPizzaStoreList.add(Store("피자헛", "1588-5588", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
-        mPizzaStoreList.add(Store("파파존스", "1577-8080", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"))
-//        mPizzaStoreList.add(Store("미스터피자", "1577-0077", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"))
-//        mPizzaStoreList.add(Store("도미뇨피자", "1577-0000", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"))
+        mPizzaStoreList.add(
+            Store(
+                "피자헛",
+                "1588-5588",
+                "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "파파존스",
+                "1577-8080",
+                "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "미스터피자",
+                "1577-0077",
+                "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "도미노피자",
+                "1577-3082",
+                "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "피자헛",
+                "1588-5588",
+                "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "파파존스",
+                "1577-8080",
+                "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "미스터피자",
+                "1577-0077",
+                "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "도미노피자",
+                "1577-3082",
+                "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "피자헛",
+                "1588-5588",
+                "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "파파존스",
+                "1577-8080",
+                "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "미스터피자",
+                "1577-0077",
+                "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"
+            )
+        )
+        mPizzaStoreList.add(
+            Store(
+                "도미뇨피자",
+                "1577-0000",
+                "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"
+            )
+        )
         // 왜 추가가 안됨???????
 
 
-        mRecyclerViewStoreAdapter = RecyclerViewStoreAdapter(context!!, R.layout.store_recycler_item, mPizzaStoreList)
+        mRecyclerViewStoreAdapter =
+            RecyclerViewStoreAdapter(context!!, R.layout.store_recycler_item, mPizzaStoreList)
         pizzaStoreRecyclerView.adapter = mRecyclerViewStoreAdapter
 
 
+//        pizzaStoreRecyclerView.setItemClickListener { parent, view, position, id ->
+//
+//            클릭된 피자가게가 어떤 가게인지, position을 이용해서 추출.
+//            val clickedStore = mPizzaStoreList[position]
+//
+//            상세 조회 화면으로 (가게데이터를 통째로 들고) 이동
+//            val myIntent = Intent(context, ViewStoreDetailActivity::class.java)
+//            Store클래스에 Serializable 속성을 부여했기 때문에 통째로 첨부 가능.
+//            myIntent.putExtra("storeData", clickedStore)
+//            startActivity(myIntent)
 
+
+//        미션: 리싸이클러뷰 항목 클릭이벤트 구현
     }
 
 }
+
